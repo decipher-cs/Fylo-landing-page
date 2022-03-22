c4a2b9c5d67bb46bc028552821ac9f12f7fa7411
de-sipher <0mu5sfm4@anonaddy.me>
2022-03-22 23:05:55 +0530

    stuff done. added css to footer and sign-in form.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,22 @@
-const elem = document.getElementsByClassName("moto-section__link")[0]
-let off = elem.offsetTop
-console.log(off)
-window.scroll(0, off)
+/* Remember scroll position */
+
+// window.onscroll = function () {
+//     let scrollPos = window.pageYOffset;
+//     localStorage.setItem("scrollPos", scrollPos);
+//     console.log(scrollPos)
+// }
+
+window.onload = function () {
+    console.log("Loaded")
+    setTimeout((e) => {
+        console.log("Remembered location : ", localStorage.getItem("scrollPos"))
+        window.scrollTo(0, localStorage.getItem("scrollPos"))
+    }
+        , 1000)
+}
+
+window.addEventListener("beforeunload", (e)=>{
+    console.log("UNLOADING!")
+    let scrollPos = window.pageYOffset;
+    localStorage.setItem("scrollPos", scrollPos);
+})
